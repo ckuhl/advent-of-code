36340410a2155dc099ed0f7ff500f128d36fc2c1
Chris Kuhl <git@ckuhl.com>
2020-12-01 13:05:51 -0700

    [01] Solve part 2 in Racket

--- 2020-12-01/2020-12-01.rkt
+++ 2020-12-01/2020-12-01.rkt
@@ -12,3 +12,14 @@
    (list->set (map (λ (x) (- TARGET x)) NUMBERS)))))
 
 (printf "Part 2~n")
+(define duals (list->set (map (λ (x) (- TARGET x)) NUMBERS)))
+(define found-tuples
+   (filter
+    (lambda (x) (set-member? duals (apply + x)))
+    (combinations NUMBERS 2)))
+
+; Print out the solution
+(*
+ (caar found-tuples)
+ (cadar found-tuples)
+ (- TARGET (caar found-tuples) (cadar found-tuples)))
