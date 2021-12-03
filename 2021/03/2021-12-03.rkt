309b9d22a8d65f98a07d3a66c28dd3631f9394fc
Chris Kuhl <git@ckuhl.com>
2021-12-03 13:01:13 -0700

    Refactor, clean up code

--- 2021/03/2021-12-03.rkt
+++ 2021/03/2021-12-03.rkt
@@ -2,10 +2,11 @@
 
 (define LINES (file->lines "2021-12-03.txt"))
 
-
 ; trie for binary numbers of identical length
 ; trie := (trie . trie) | number | empty
-; left = 0, right = 1
+; car = 0, cdr = 1
+; number = a count of results for this path
+; empty = no results (used when the other half of the pair contains a number)
 
 ; Insert / update a binary number into a trie
 (define (trie-insert str trie)
@@ -19,7 +20,7 @@
     [else (cons (car trie) (trie-insert (substring str 1) (cdr trie)))])) ; string starts with 1
 
 
-; Count size of trie
+; Count the number of items in the trie
 (define (trie-count trie)
   (cond
     [(pair? trie) (+ (trie-count (car trie)) (trie-count (cdr trie)))]
@@ -37,17 +38,14 @@
     [(number? trie1) (+ trie1 trie2)]
 
     ; Recurse
-    [else (cons (trie-merge (car trie1) (car trie2)) (trie-merge (cdr trie1) (cdr trie2)))]))
-
-; Helper: Invert a list of #\0 and #\1 characters
-(define (invert-string charlist)
-  (cond
-   [(empty? charlist) charlist]
-   [(eq? (first charlist) #\0) (cons #\1 (invert-string (rest charlist)))]
-   [else (cons #\0 (invert-string (rest charlist)))]))
+    [else
+     (cons
+      (trie-merge (car trie1) (car trie2))
+      (trie-merge (cdr trie1) (cdr trie2)))]))
 
 ; Helper: Convert a list of #\0 and #\1 into the number they represent
 (define (charlist->number lst) (string->number (list->string lst) 2))
+
 
 ; Trie with _all_ of the binary strings inserted
 (define BIT-TRIE (foldl trie-insert empty LINES))
@@ -56,67 +54,50 @@
 (printf "Part 1~n")
 
 
-; Find the most prominent first bit in a trie
-(define (trie-prominent-bit trie)
-  (if
-   (> (trie-count (car trie)) (trie-count (cdr trie)))
-   #\0
-   #\1))
+; Compare the size of the trie branches using cmp
+(define (trie-branch-cmp trie cmp)
+  (cmp (trie-count (car trie)) (trie-count (cdr trie))))
 
-; Recursively find the most prominent bit in a trie
-(define (trie-prominent-bits trie)
+; Recursively find the most prominent bit in a trie according to cmp
+(define (trie-prominent-bits trie cmp)
   (if
-   ; Base case: We've reached the bottom of the trie
+   ; Base case
    (number? trie) empty
 
    ; Otherwise, recurse
    (cons
-    (trie-prominent-bit trie)
-    (trie-prominent-bits (trie-merge (car trie) (cdr trie))))))
+    (if (trie-branch-cmp trie cmp) #\0 #\1)
+    (trie-prominent-bits (trie-merge (car trie) (cdr trie)) cmp))))
 
-
-(define gamma-charlist (trie-prominent-bits BIT-TRIE))
-(define epsilon-charlist (invert-string gamma-charlist))
-
+  
 ; Solution: 1307354
 (*
- (charlist->number gamma-charlist)
- (charlist->number epsilon-charlist))
+ (charlist->number (trie-prominent-bits BIT-TRIE >)) ; Gamma (most common bits)
+ (charlist->number (trie-prominent-bits BIT-TRIE <=))) ; Epsilon (least common bits)
 
 
 ; ==============================================================================
 (printf "Part 2~n")
 
-; Follow a trie, taking the most popular branch at each step
-;  In the case of ties, take #\1
-(define (trie-most-popular trie)
+; Follow a trie, taking the branch specified by cmp
+
+(define (trie-branch trie cmp)
   (cond
     ; Base case
     [(number? trie) empty]
 
+    ; Singular case: Take branch with one
+    [(empty? (car trie)) (cons #\1 (trie-branch (cdr trie) cmp))]
+    [(empty? (cdr trie)) (cons #\0 (trie-branch (car trie) cmp))]
+
     ; Recursive cases
-    [(> (trie-count (car trie)) (trie-count (cdr trie)))
-     (cons #\0 (trie-most-popular (car trie)))]
-    [else (cons #\1 (trie-most-popular (cdr trie)))]))
-
-
-; Follow a trie, taking the least popular branch at each step
-;  In the case of ties, take #\0
-(define (trie-least-popular trie)
-  (cond
-    ; Base case(s)
-    [(number? trie) empty]
-
-    ; When there's one left, take that branch
-    [(= 1 (trie-count trie)) (if (= 1 (trie-count (car trie))) (cons #\0 (trie-least-popular (car trie))) (cons #\1 (trie-least-popular (cdr trie))))]
-
-    ; Standard recursive cases
-    [(> (trie-count (car trie)) (trie-count (cdr trie)))
-     (cons #\1 (trie-least-popular (cdr trie)))]
-    [else (cons #\0 (trie-least-popular (car trie)))]))
+    [(cmp (trie-count (car trie)) (trie-count (cdr trie)))
+     (cons #\0 (trie-branch (car trie) cmp))]
+    [else
+     (cons #\1 (trie-branch (cdr trie) cmp))]))
 
 
 ; Solution: 482500
 (*
- (charlist->number (trie-most-popular BIT-TRIE))
- (charlist->number (trie-least-popular BIT-TRIE)))
+ (charlist->number (trie-branch BIT-TRIE >)) ; Oxygen generator rating (most common)
+ (charlist->number (trie-branch BIT-TRIE <=))) ; CO2 scrubber rating (least common)
