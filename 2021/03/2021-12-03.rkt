dc805445fa3ee3e4cfad7726b020efb6f22d51ed
Chris Kuhl <git@ckuhl.com>
2021-12-03 09:28:18 -0700

    2021 day 3: Solve part 1 with a trie

--- 2021/03/2021-12-03.rkt
+++ 2021/03/2021-12-03.rkt
@@ -3,47 +3,6 @@
 (define LINES (file->lines "2021-12-03.txt"))
 
 
-(printf "Part 1~n")
-
-
-(define (bit-count lst index [count 0])
-  (if (empty? lst) count
-      (bit-count (rest lst) index (+ count (string->number (substring (first lst) index (add1 index)))))))
-
-(define bits
-  (list
-   (bit-count LINES 0)
-   (bit-count LINES 1)
-   (bit-count LINES 2)
-   (bit-count LINES 3)
-   (bit-count LINES 4)
-   (bit-count LINES 5)
-   (bit-count LINES 6)
-   (bit-count LINES 7)
-   (bit-count LINES 8)
-   (bit-count LINES 9)
-   (bit-count LINES 10)
-   (bit-count LINES 11)))
-
-(define total (length LINES))
-
-(define (most-common-bit lst total)
-  (map (lambda (x) (if (>= (* 2 x) total) 1 0)) lst))
-
-(define (least-common-bit lst total)
-  (map (lambda (x) (if (>= (* 2 x) total) 0 1)) lst))
-
-
-(define gamma (foldl string-append "" (reverse (map number->string (most-common-bit bits total)))))
-(define epsilon (foldl string-append "" (reverse (map number->string (least-common-bit bits total)))))
-
-
-(*
- (string->number gamma 2)
- (string->number epsilon 2))
-
-(printf "Part 2~n")
-
 ; trie for binary numbers of identical length
 ; trie := (trie . trie) | (number . number) | empty
 ; Left = 0, right = 1
@@ -51,19 +10,81 @@
 ; Insert / update a binary number into a trie
 (define (trie-insert str trie)
   (cond
+    ; Base case: End of string, update count
     [(string=? "" str) (if (empty? trie) 1 (add1 trie))]
-    [(empty? trie) (trie-insert str (cons empty empty))]
+
+    ; trie branch empty: Insert deeper branch and retry
+    [(empty? trie) (trie-insert str (cons '() '()))]
     [(eq? #\0 (string-ref str 0)) (cons (trie-insert (substring str 1) (car trie)) (cdr trie))] ; string starts with 0
     [else (cons (car trie) (trie-insert (substring str 1) (cdr trie)))])) ; string starts with 1
 
+; Trie with _all_ of the binary strings inserted
+(define BIT-TRIE (foldl trie-insert empty LINES))
 
-(define int-trie (foldl trie-insert empty LINES))
-
+; Count size of trie
 (define (trie-count trie)
   (cond
     [(pair? trie) (+ (trie-count (car trie)) (trie-count (cdr trie)))]
     [(empty? trie) 0]
     [else trie]))
+
+
+(printf "Part 1~n")
+
+; Merge two tries into one
+(define (trie-merge trie1 trie2)
+  (cond
+    ; Base case(s): Take one trie
+    [(empty? trie1) trie2]
+    [(empty? trie2) trie1]
+
+    ; Base case: At terminal depth so we know both must be numbers here if one is
+    [(number? trie1) (+ trie1 trie2)]
+
+    ; Recurse
+    [else (cons (trie-merge (car trie1) (car trie2)) (trie-merge (cdr trie1) (cdr trie2)))]))
+
+
+; Find the most prominent first bit in a trie
+(define (trie-prominent-bit trie)
+  (if
+   (> (trie-count (car trie)) (trie-count (cdr trie)))
+   #\0
+   #\1))
+
+(define (trie-prominent-bits trie)
+  (if
+   ; Base case: We've reached the bottom of the trie
+   (number? trie) empty
+
+   ; Otherwise, recurse
+   (cons
+    (trie-prominent-bit trie)
+    (trie-prominent-bits (trie-merge (car trie) (cdr trie))))))
+
+; Helper: Invert a list of #\0 and #\1 characters
+(define (invert-string charlist)
+  (cond
+   [(empty? charlist) charlist]
+   [(eq? (first charlist) #\0) (cons #\1 (invert-string (rest charlist)))]
+   [else (cons #\0 (invert-string (rest charlist)))]))
+
+
+(define (charlist->number lst) (string->number (list->string lst) 2))
+
+(define gamma-charlist (trie-prominent-bits BIT-TRIE))
+(define gamma (charlist->number gamma-charlist))
+
+(define epsilon-charlist (invert-string gamma-charlist))
+(define epsilon (charlist->number epsilon-charlist))
+
+(*
+ gamma
+ epsilon)
+
+; Expected: 1307354
+
+(printf "Part 2~n")
 
 (define (trie-most-popular trie)
   (cond
@@ -91,5 +112,6 @@
 
 
 (*
- (string->number (list->string (trie-most-popular int-trie)) 2)
- (string->number (list->string (trie-least-popular int-trie)) 2))
+ (charlist->number (trie-most-popular BIT-TRIE))
+ (charlist->number (trie-least-popular BIT-TRIE)))
+; Expected: 482500
