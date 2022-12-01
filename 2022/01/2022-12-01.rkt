b16bcb7741ea3792526a82b6e9b4bc593d0602c9
Chris Kuhl <git@ckuhl.com>
2022-11-30 22:37:41 -0700

    [2022][01] Solution

--- 2022/01/2022-12-01.rkt
+++ 2022/01/2022-12-01.rkt
@@ -1,11 +1,41 @@
 #lang racket
 
-(define INPUT (file->list "2022-12-01.txt"))
+(define INPUT (file->lines "2022-12-01.txt"))
 
+(define (take-first-sublist lst)
+  (cond
+    [(empty? lst) empty]
+    [(equal? (first lst) "") empty]
+    [else (cons (string->number (first lst)) (take-first-sublist (rest lst)))]))
+
+(define (skip-first-sublist lst)
+  (cond
+    [(empty? lst) empty]
+    [(equal? (first lst) "") (rest lst)]
+    [else (skip-first-sublist (rest lst))]))
+
+(define (sublist-ify lst)
+  (if
+   (empty? lst)
+   empty
+  (cons
+   (take-first-sublist lst)
+   (sublist-ify (skip-first-sublist lst)))))
+
+(define SUBLISTS (sublist-ify INPUT))
+
+(define CALORIES (map (lambda (x) (apply + x)) SUBLISTS))
 
 (printf "Part 1~n")
-; TODO: Solve part 1
+
+(apply max CALORIES)
+
 
 (printf "Part 2~n")
-; TODO: Solve part 2
 
+(define top-calories (sort CALORIES >))
+
+(+
+ (first top-calories)
+ (second top-calories)
+ (third top-calories))
