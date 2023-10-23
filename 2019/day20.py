b2f7b8649926829ca79974b3e7931aaef59574c0
Chris Kuhl <git@ckuhl.com>
2023-10-22 18:36:30 -0600

    [2019][20] Solve part 2

--- 2019/day20.py
+++ 2019/day20.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+import dataclasses
 
 EXAMPLE1 = """         A           
          A           
@@ -58,6 +58,44 @@ YN......#               VT..#....QG
            B   J   C               
            U   P   P               """
 
+EXAMPLE3 = """             Z L X W       C                 
+             Z P Q B       K                 
+  ###########.#.#.#.#######.###############  
+  #...#.......#.#.......#.#.......#.#.#...#  
+  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###  
+  #.#...#.#.#...#.#.#...#...#...#.#.......#  
+  #.###.#######.###.###.#.###.###.#.#######  
+  #...#.......#.#...#...#.............#...#  
+  #.#########.#######.#.#######.#######.###  
+  #...#.#    F       R I       Z    #.#.#.#  
+  #.###.#    D       E C       H    #.#.#.#  
+  #.#...#                           #...#.#  
+  #.###.#                           #.###.#  
+  #.#....OA                       WB..#.#..ZH
+  #.###.#                           #.#.#.#  
+CJ......#                           #.....#  
+  #######                           #######  
+  #.#....CK                         #......IC
+  #.###.#                           #.###.#  
+  #.....#                           #...#.#  
+  ###.###                           #.#.#.#  
+XF....#.#                         RF..#.#.#  
+  #####.#                           #######  
+  #......CJ                       NM..#...#  
+  ###.#.#                           #.###.#  
+RE....#.#                           #......RF
+  ###.###        X   X       L      #.#.#.#  
+  #.....#        F   Q       P      #.#.#.#  
+  ###.###########.###.#######.#########.###  
+  #.....#...#.....#.......#...#.....#.#...#  
+  #####.#.###.#######.#######.###.###.#.#.#  
+  #.......#.......#.#.#.#.#...#...#...#.#.#  
+  #####.###.#####.#.#.#.#.###.###.#.###.###  
+  #.......#.....#.#...#...............#...#  
+  #############.#.#.###.###################  
+               A O F   N                     
+               A A D   M                     """
+
 
 def parse(string_or_default: str | None = None) -> NotImplemented:
     if string_or_default is None:
@@ -78,11 +116,12 @@ def parse(string_or_default: str | None = None) -> NotImplemented:
 def extract_portals(mapping: dict[tuple[int, int], str]) -> dict[tuple[int, int], str]:
     """
     Given an unprocessed mapping, return a new mapping of:
-    portal_pos: portal_tag
-    >>> extract_portals(parse(EXAMPLE1))
-    >>> {(9, 2): 'AA', (9, 6): 'BC', (2, 8): 'BC', (6, 10): 'DE', (11, 12): 'FG', (2, 13): 'DE', (2, 15): 'FG', (13, 16): 'ZZ'}
+    `portal_pos`: `portal_name`
 
-    Since tags are always in whitespace, we will only add when we find the middle of a tag, to avoid duplication.
+    > extract_portals(parse(EXAMPLE1))
+    > {(9, 2): 'AA', (9, 6): 'BC', (2, 8): 'BC', (6, 10): 'DE', (11, 12): 'FG', (2, 13): 'DE', (2, 15): 'FG', (13, 16): 'ZZ'}
+
+    Since tags are always in two characters, we will only add when we find the middle of a tag, to avoid duplication.
     """
     tags = {}
     for k, v in mapping.items():
@@ -103,98 +142,120 @@ def extract_portals(mapping: dict[tuple[int, int], str]) -> dict[tuple[int, int]
     return tags
 
 
-def construct_weighted_graph(
+def portal_to_portal(portals: dict[tuple[int, int], str]) -> dict[tuple[int, int] | str, tuple[int, int]]:
+    """
+    Given the above portal-to-tag mapping, create a mapping of point-to-point of portal positions; for AA and ZZ, store
+    the key as the string, as we'll only ever compare those two directly.
+    """
+    p2p = {}
+    for k, v in portals.items():
+        if v in p2p:
+            dest = p2p.pop(v)
+            p2p[k] = dest
+            p2p[dest] = k
+        else:
+            p2p[v] = k
+    return p2p
+
+
+def get_outer_portal_fn(portals: dict[tuple[int, int], str]):
+    """
+    Portals can be along: One of four rows, or one of four columns;
+    Outer portals are in the first and last row and column.
+    Thus, we can sort the portals by x, take the greatest and least;
+    Then sort again by y, take the greatest and least;
+    This gives us the outer portals, and the remainder, inner portals.
+
+    This returns a predicate that can be used to determine if a given portal is on the outside or the inside.
+    """
+    min_x = min(x[0] for x in portals.keys())
+    max_x = max(x[0] for x in portals.keys())
+    min_y = min(x[1] for x in portals.keys())
+    max_y = max(x[1] for x in portals.keys())
+    return lambda x: x[0] == min_x or x[0] == max_x or x[1] == min_y or x[1] == max_y
+
+
+@dataclasses.dataclass(eq=True, frozen=True)
+class Point:
+    pos: tuple[int, int]
+    depth: int
+
+
+def bfs_graph(
         mapping: dict[tuple[int, int], str],
         portals: dict[tuple[int, int], str],
-) -> dict[str, list[tuple[str, int]]]:
+        with_depth: bool = False,
+) -> int | None:
     """
-    Given a donut map and a mapping of portals:
-    1. Select a portal.
-    2. Add an entry to our list of portal routes.
-    3. BFS to every other portal reachable.
-        a. For each portal reached, add the portal and number of steps to reach it to the routes.
-    4. Return this mapping.
+    Turns out constructing the graph is more harm than help.
+    Let's just BFS through the graph at this point.
     """
-    graph = defaultdict(list)
-    for pos, tag in portals.items():
-        curr_frontier, next_frontier = [pos], []
-        steps = 0
-        seen = set()
-        while curr_frontier or next_frontier:
-            if not curr_frontier:
-                curr_frontier, next_frontier = next_frontier, []
-                steps += 1
+    is_outer = get_outer_portal_fn(portals)
+    p2p = portal_to_portal(portals)
 
-            prospect = curr_frontier.pop()
-            if prospect in seen:
+    curr_frontier: set[Point] = {Point(pos=p2p["AA"], depth=0)}
+    next_frontier: set[Point] = set()
+    steps = 0
+    seen: set[Point] = set()
+
+    while curr_frontier or next_frontier:
+        if not curr_frontier:
+            curr_frontier, next_frontier = next_frontier, set()
+            steps += 1
+
+        prospect = curr_frontier.pop()
+        prospect: Point
+
+        if mapping.get(prospect.pos) != ".":
+            continue
+
+        if prospect in seen:
+            continue
+        else:
+            seen.add(prospect)
+
+        if prospect.pos == p2p["ZZ"] and not with_depth:
+            return steps
+        if prospect.pos == p2p["ZZ"] and with_depth and prospect.depth == 0:
+            return steps
+
+        if prospect.pos in p2p:
+            if is_outer(prospect.pos) and with_depth and prospect.depth == 0:
+                # Can't return out beyond initial depth
                 continue
-
+            elif is_outer(prospect.pos):
+                next_frontier.add(Point(pos=p2p[prospect.pos], depth=prospect.depth - 1))
             else:
-                seen.add(prospect)
+                next_frontier.add(Point(pos=p2p[prospect.pos], depth=prospect.depth + 1))
 
-            if mapping.get(prospect) != ".":
-                continue
+        next_frontier.add(Point(pos=(prospect.pos[0] + 1, prospect.pos[1]), depth=prospect.depth))
+        next_frontier.add(Point(pos=(prospect.pos[0] - 1, prospect.pos[1]), depth=prospect.depth))
+        next_frontier.add(Point(pos=(prospect.pos[0], prospect.pos[1] + 1), depth=prospect.depth))
+        next_frontier.add(Point(pos=(prospect.pos[0], prospect.pos[1] - 1), depth=prospect.depth))
 
-            if prospect in portals and portals[prospect] != tag:
-                graph[tag].append((portals[prospect], steps,))
-
-            next_frontier.append((prospect[0] + 1, prospect[1]))
-            next_frontier.append((prospect[0] - 1, prospect[1]))
-            next_frontier.append((prospect[0], prospect[1] + 1))
-            next_frontier.append((prospect[0], prospect[1] - 1))
-
-    return graph
+    return None
 
 
-def minimum_cost_weight_graph(
-        graph: dict[str, list[tuple[str, int]]],
-        start: str,
-        stop: str,
-) -> int:
-    """
-    Given a graph mapping costs between nodes, what is the minimum cost to get from the start to the end?
-    This is a variation on BFS:
-    - We need to track cost
-    - We need to track routes (to catch cycles)
-    - We end early when all routes in the frontier (and next frontier) are higher-cost than the shortest known route
-    """
-    shortest_route = float("inf")
-    frontier, next_frontier = {(start,): 0}, {}
-    while frontier or next_frontier:
-        if not frontier:
-            frontier = {k: v for k, v in next_frontier.items()}
-            next_frontier = {}
-
-        candidate, cost = frontier.popitem()
-        for neighbour, travel_cost in graph[candidate[-1]]:
-            if neighbour in candidate:
-                # Cycling; don't do that
-                continue
-            elif neighbour == stop:
-                # We found the end; is it a good one?
-                shortest_route = min(shortest_route, cost + travel_cost)
-                continue
-
-            # Continue iterating
-            route = tuple(list(candidate) + [neighbour])
-            next_frontier[route] = min(next_frontier.get(route, float("inf")), cost + travel_cost + 1)
-
-    return shortest_route
+def solve_donut(string_or_default: str | None = None, with_depth=False):
+    raw = parse(string_or_default)
+    portals = extract_portals(raw)
+    return bfs_graph(mapping=raw, portals=portals, with_depth=with_depth)
 
 
 def part1(string_or_default: str | None = None):
-    raw = parse(string_or_default)
-    portals = extract_portals(raw)
-    weighted_graph = construct_weighted_graph(raw, portals)
-    return minimum_cost_weight_graph(weighted_graph, "AA", "ZZ")
+    return solve_donut(string_or_default, with_depth=False)
 
 
-def part2():
-    """Todo: Solve part 2 once off the plane"""
-    raise NotImplementedError
+def part2(string_or_default: str | None = None):
+    return solve_donut(string_or_default, with_depth=True)
 
 
 if __name__ == "__main__":
     assert part1(EXAMPLE1) == 23
     assert part1(EXAMPLE2) == 58
     assert part1() == 686
+    assert part2(EXAMPLE1) == 26
+    # assert part2(EXAMPLE2) is None  # Example 2 does not have a valid route; return the sentinel value
+    # FIXME: In tweaking the code to work with EXAMPLE3, I broke loop detection... oops
+    assert part2(EXAMPLE3) == 396
+    assert part2() == 8384
