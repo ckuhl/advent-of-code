9061d97f71b2d4ab69712c095588036b6c3f88c2
Chris Kuhl <git@ckuhl.com>
2023-12-01 10:30:09 -0700

    [2023][01] Complet second part

--- 2023/01.lua
+++ 2023/01.lua
@@ -1,27 +1,70 @@
--- Template file to copy-paste for each day
-problem = io.lines("inputs/01.txt")
-example = io.lines("inputs/01-example1.txt")
-
-function BuildArray(...)
-  local arr = {}
-  for v in ... do
-    arr[#arr + 1] = v
-  end
-  return arr
-end
-
-local problem = BuildArray(problem)
-local total = 0
-for l = 1, #problem do
-  local s = problem[l]
-  total = total + tonumber(s:match('^%a*(%d)') .. s:match('(%d)%a*$'))
+function part1(fileName)
+	local total = 0
+	for l in io.lines(fileName) do
+		total = total + tonumber(l:match('^%a*(%d)') .. l:match('(%d)%a*$'))
+	end
+	return total
 end
 
 -- Part 1:
--- Example: 142
--- Problem: 54951
-print(total)
+assert(part1('inputs/01-example1.txt') == 142)
+assert(part1('inputs/01.txt') == 54951)
 
 -- Part 2:
 -- Excuse me? Lua does not have a regex OR???
+
+local numbers = {
+	'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '%d'
+}
+
+function findFirst(s)
+	local earliest = s:len()
+	local value = nil
+	for n = 1, #numbers do
+		for c, d in s:gmatch('()' .. numbers[n] .. '()') do
+			-- if this is a new "earliest" number
+			if d ~= nil and d - 1 <= earliest then
+				earliest = d - 1
+				if n == 10 then
+					value = tonumber(s:match('^%a*(%d)'))
+				else
+		value = n
+				end
+			end
+		end
+	end
+	return value
+end
+
+-- TODO: Consider merging these two into a "find all" and return offsets
+function findLast(s)
+	local latest = 0
+	local value = nil
+	for n = 1, #numbers do
+		for c, d in s:gmatch('()' .. numbers[n] .. '()') do
+			-- if this is a new "earliest" number
+			if c ~= nil and c >= latest then
+				latest = c
+				if n == 10 then
+					value = tonumber(s:match('(%d)%a-$'))
+				else
+					-- We set up the word regexes to match their index
+					value = n
+				end
+			end
+		end
+	end
+	return value
+end
+
+function part2(fileName)
+	local total = 0
+	for l in io.lines(fileName) do
+		total = total + tonumber(tostring(findFirst(l)) .. tostring(findLast(l)))
+	end
+	return total
+end
+
+assert(part2("inputs/01-example2.txt") == 281)
+assert(part2("inputs/01.txt") == 55218)
 
