8e0df2f802fd47655927e72826bdbf01efbc32d6
Chris Kuhl <git@ckuhl.com>
2023-12-03 11:15:12 -0700

    [2023][03] Complete second part

--- 2023/03.lua
+++ 2023/03.lua
@@ -2,73 +2,55 @@
 aoc = require("aoc")
 
 -- Part 1 ---------------------------------------------------------------------
--- Helper: Convert file to list of points
--- Format: "x:y" : <char>
-function fileToPoints(fileName)
-    local t = {}
-    local y = 0
-    for l in io.lines(fileName) do
-        y = y + 1
-        local x = 0
-        for i = 1, #l do
-            x = x + 1
-            local c = l:sub(i, i)
-            t[tostring(x) .. ";" .. tostring(y)] = c
-        end
-    end
-    return t
+function isSymbol(str)
+	if str == nil then
+		-- Case: Out of bounds
+		return false
+	elseif str == "." then
+		-- Case: Filter out 'empty' values
+		return false
+	else
+		-- Case: Numbers are not symbols
+		return tonumber(str) == nil
+	end
 end
 
-function isNonSymbol(str)
-    if str == nil then
-        return false
-    end
-    local symbols = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "." }
-    for _, l in ipairs(symbols) do
-        symbols[l] = true
-    end
-    if symbols[str] then
-        return false
-    else
-        return true
-    end
-end
-
--- Return true if any adjacent symbols (i.e. any non-'.', non-integer characters)
 function adjacentSymbols(mapping, px, py)
-    for y = py - 1, py + 1 do
-        for x = px - 1, px + 1 do
-            if not (y == py and x == px) and isNonSymbol(mapping[tonumber(x) .. ";" .. tonumber(y)]) then
-                return true
-            end
-        end
-    end
-    return false
+	-- Return true if any adjacent symbols (i.e. any non-'.', non-integer characters)
+	for y = py - 1, py + 1 do
+		for x = px - 1, px + 1 do
+			-- Guard on checking itself
+			if not (y == py and x == px) and isNonSymbol(mapping[tonumber(x) .. ";" .. tonumber(y)]) then
+				return true
+			end
+		end
+	end
+	return false
 end
 
-function specialCharacterInRange(mapping, py, px1, px2)
-    for x = px1, px2 do
-        if adjacentSymbols(mapping, x, py) then
-            return true
-        end
-    end
-    return false
+function specialCharacterInRange(mapping, px1, px2, py)
+	-- FIXME: Fold this into the above function
+	for x = px1, px2 do
+		if adjacentSymbols(mapping, x, py) then
+			return true
+		end
+	end
+	return false
 end
 
 function part1(fileName)
-    local mapping = fileToPoints(fileName)
-    local partsSum = 0
-    local y = 0
-    for l in io.lines(fileName) do
-        y = y + 1
-        for x_start, x_end_past in l:gmatch("()%d+()") do
-            local x_end = x_end_past - 1
-            if specialCharacterInRange(mapping, y, x_start, x_end) then
-                partsSum = partsSum + tonumber(string.sub(l, x_start, x_end))
-            end
-        end
-    end
-    return partsSum
+	local mapping = aoc.fileToPoints(fileName)
+	local partsSum = 0
+	local y = 0
+	for l in io.lines(fileName) do
+		y = y + 1
+		for x_start, x_end_past in l:gmatch("()%d+()") do
+			if specialCharacterInRange(mapping, x_start, x_end_past - 1, y) then
+				partsSum = partsSum + tonumber(l:sub(x_start, x_end_past - 1))
+			end
+		end
+	end
+	return partsSum
 end
 
 assert(part1("inputs/03-example1.txt") == 4361)
@@ -76,13 +58,47 @@ assert(part1("inputs/03.txt") == 517021)
 
 -- Part 2 ---------------------------------------------------------------------
 
--- TODO: The rest of this is a tomorrow problem
-
-function part2(fileName)
-    --for l in io.lines(fileName) do
-    --end
-    return -1
+function findAdjacentStar(mapping, px1, px2, py)
+	-- Helper: Given a number (i.e. a range of x values it occupies), return a star if adjacent
+	for y = py - 1, py + 1 do
+		for x = px1 - 1, px2 + 1 do
+			local key = tonumber(x) .. ";" .. tonumber(y)
+			if not (y == py and x == px) and mapping[key] == "*" then
+				return key
+			end
+		end
+	end
 end
 
-assert(part2("inputs/03-example2.txt") == 1)
-assert(part2("inputs/03.txt") == 1)
+function part2(fileName)
+	-- 1. Find number.
+	-- 2. If adjacent to '*':
+	-- 2.1. Does star exist in table (i.e. have we already seen a number by this star?)
+	-- 2.1.1. Multiply value by star entry, add to score.
+	-- 2.1.2. Add entry to table (star_pos) = number
+	local mapping = aoc.fileToPoints(fileName)
+	local stars = {}
+	local gearRatioSum = 0
+
+	local y = 0
+	for l in io.lines(fileName) do
+		y = y + 1
+		for x_start, x_end_past in l:gmatch("()%d+()") do
+			local number = tonumber(l:sub(x_start, x_end_past - 1))
+			local pos = findAdjacentStar(mapping, x_start, x_end_past - 1, y)
+			if pos ~= nil then
+				if stars[pos] == nil then
+					stars[pos] = number
+				else
+					gearRatioSum = gearRatioSum + stars[pos] * number
+					-- Set the star position to a sentinel to ensure there's no three-number stars
+					stars[pos] = 0 / 0
+				end
+			end
+		end
+	end
+	return gearRatioSum
+end
+
+assert(part2("inputs/03-example1.txt") == 467835)
+assert(part2("inputs/03.txt") == 81296995)
--- 2023/aoc.lua
+++ 2023/aoc.lua
@@ -2,24 +2,40 @@ local aoc = {}
 
 --- Helper: Dump table into string for easy printing out
 function aoc.dumpTable(t)
-    local s = "{ "
-    for k, v in pairs(t) do
-        s = s .. "[" .. tostring(k) .. "] = " .. tostring(v) .. ", "
-    end
-    s = s .. "}"
-    return s
+	local s = "{ "
+	for k, v in pairs(t) do
+		s = s .. "[" .. tostring(k) .. "] = " .. tostring(v) .. ", "
+	end
+	s = s .. "}"
+	return s
 end
 
 --- Helper: Split string into table based on given separator (or spaces if not separator given)
 function aoc.split(inputString, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t = {}
-    for s in string.gmatch(inputString, "([^" .. sep .. "]+)") do
-        table.insert(t, s)
-    end
-    return t
+	if sep == nil then
+		sep = "%s"
+	end
+	local t = {}
+	for s in string.gmatch(inputString, "([^" .. sep .. "]+)") do
+		table.insert(t, s)
+	end
+	return t
+end
+
+--- Helper: Given a filename, convert it to a table: "x;y" to character
+function aoc.fileToPoints(fileName)
+	local t = {}
+	local y = 0
+	for l in io.lines(fileName) do
+		y = y + 1
+		local x = 0
+		for i = 1, #l do
+			x = x + 1
+			local c = l:sub(i, i)
+			t[tostring(x) .. ";" .. tostring(y)] = c
+		end
+	end
+	return t
 end
 
 return aoc
